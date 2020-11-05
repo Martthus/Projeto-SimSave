71a8097e1d0f205b1097e78e6549c3955d53b45e
Matheus Martins <66175793+Martthus@users.noreply.github.com>
2020-11-05 12:33:38 -0300

    ..

--- src/app/screens/login/login.module.ts
+++ src/app/screens/login/login.module.ts
@@ -1,10 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button'
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { LoginComponent } from './login.component';
 
 @NgModule({
@@ -13,8 +9,7 @@ import { LoginComponent } from './login.component';
     CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
-    CommonModule,
-    
+    CommonModule
   ],
   providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }],
   bootstrap: [LoginComponent]
